82e4541cc68d2a6650d68c44f77b1e1e65578ebd
Kunakorn <kunakorn.ph98@gmail.com>
2024-09-04 14:34:48 +0700

    feat: add cors

--- index.js
+++ index.js
@@ -2,11 +2,20 @@ import express from "express";
 import postRouter from "./api/posts.js";
 import authRouter from "./api/auth.js";
 import bodyParser from "body-parser";
+import cors from "cors";
 
 const app = express();
 const port = 4000;
 
 app.use(bodyParser.json());
+
+app.use(
+  cors({
+    origin: "https://frontend-block-post.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("API is working on main route");
